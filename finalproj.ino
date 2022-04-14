e58dbedfea29fbc9b994cb6fbd1467a3543ed49a
Anais Lawson <anl2140@columbia.edu>
2022-04-14 10:43:02 -0400

    added people data

--- finalproj.ino
+++ finalproj.ino
@@ -5,19 +5,58 @@
   Modification: 2020/07/11
 **********************************************************************/
 #include <ESP32Servo.h>
+#include <iostream>
+#include <map>
+#include <cstring>
 
+using namespace std;
 
+using std::cout; using std::cin;
+using std::endl; using std::string;
+using std::map; using std::copy;
+
+map<string, string> sunny = {"Anais", "yes",},
+                                {"Sahory", "yes",},
+                                {"Andre", "yes",},
+                                {"Mark", "yes",},
+                                {"Gen", "yes",},
+                                {"Kennedi", "yes",}}; 
+
+map<string, string> rain = {"Anais", "no",},
+                                {"Sahory", "yes",},
+                                {"Andre", "no",},
+                                {"Mark", "no",},
+                                {"Gen", "no",},
+                                {"Kennedi", "no",}}; 
+                                
+
+map<string, string> thunderstorm = {"Anais", "no",},
+                                {"Sahory", "yes",},
+                                {"Andre", "yes",},
+                                {"Mark", "no",},
+                                {"Gen", "yes",},
+                                {"Kennedi", "no",}}; 
+
+map<string, string> cloudy = {{"Anais", "yes",},
+                                {"Sahory", "yes",},
+                                {"Andre", "no",},
+                                {"Mark", "yes",},
+                                {"Gen", "yes",},
+                                {"Kennedi", "yes",}};                            
 // Connect the port of the stepper motor driver
 int outPorts[] = {15, 27, 26, 25};
-
+int index = 0;
+int count = 0; 
+const String people[6] = {"Anais, Sahory, Andre, Mark, Gen, Kennedi"};
 
 Servo myservoOne;  // create servo object to control a servo
 Servo myservoTwo;  // create servo object to control a servo
 
 int posVal = 0;    // variable to store the servo position
 int servoPin = 15; // Servo motor pin
-
+char name[] = people[0];
 void setup() {
+  //motor setup  
   // set pins to output
   for (int i = 0; i < 4; i++) {
     pinMode(outPorts[i], OUTPUT);
@@ -32,12 +71,34 @@ void setup() {
 
 void loop()
 {
- 
+   
   delay(100); // this speeds up the simulation  
-  //read joystick  
-  int joyVal = analogRead(12);
-    
-
+  //read potentiometer  
+  int potVal = analogRead(12);
+  //read joystick
+  int joyVal = analogRead(27);
+  //change person  
+  if (joyVal > 2000 && count == 0) {
+      
+      //next name
+      index = (index + 1) % 6;
+  }  
+  else{
+      count = 0;
+  }
+  //change weather
+  if (potVal < 1000 and potVal > 100){
+      //sun
+  }
+  if (potVal < 2000 and potVal > 1000){
+      //rain
+  }
+  if (potVal < 3000 and potVal > 2000){
+      //thunderstorm
+  }
+  if (potVal > 3000){
+      //cloudy
+  }
 
   //STEPPER MOTOR
   //Should only rotate if there is "sunny"  
